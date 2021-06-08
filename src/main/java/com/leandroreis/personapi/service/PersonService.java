1dea08dc05368323371ab94817b5b86b07829d79
leandrole8 <leandro_le8@hotmail.com>
2021-06-08 19:26:12 -0300

    Criado o pacote mapper, interface PersonMApper e implantando o metodo get All

--- src/main/java/com/leandroreis/personapi/service/PersonService.java
+++ src/main/java/com/leandroreis/personapi/service/PersonService.java
@@ -3,26 +3,42 @@ package com.leandroreis.personapi.service;
 import com.leandroreis.personapi.dto.MessageResponseDTO;
 import com.leandroreis.personapi.dto.request.PersonDTO;
 import com.leandroreis.personapi.entity.Person;
+import com.leandroreis.personapi.mapper.PersonMapper;
 import com.leandroreis.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final personMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository){
+
         this.personRepository = personRepository;
     }
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
-        
-        Person savedPerson = personRepository.save(personDTO);
+
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
-                .message("Created personDTO with ID " + savedPerson.getId())
+                .message("Created person with ID " + savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
