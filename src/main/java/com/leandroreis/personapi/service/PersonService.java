bb398b054238aa316d08e962515f69e889b8aecd
leandrole8 <leandro_le8@hotmail.com>
2021-06-09 18:00:58 -0300

    Criado o metodo post para update de pessoas

--- src/main/java/com/leandroreis/personapi/service/PersonService.java
+++ src/main/java/com/leandroreis/personapi/service/PersonService.java
@@ -56,8 +56,25 @@ public class PersonService {
         personRepository.deleteById(id);
     }
 
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfExists(id);
+
+        Person personToUpdate = personMapper.toModel(personDTO);
+
+        Person updatedPerson = personRepository.save(personToUpdate);
+        return createMessageResponse(updatedPerson.getId(), "Updated person with ID ");
+    }
+
     private Person verifyIfExists(Long id) throws PersonNotFoundException {
         return personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
     }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
+    }
+
 }
